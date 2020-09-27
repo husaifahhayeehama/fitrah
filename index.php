e4d9b0335183837b8d75daeb596a36a8c3364be5
Husaifah Hayeehama <husaifahhayeehama@gmail.com>
2020-09-27 14:51:33 -0700

    Test template

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link href="font/fontawesome/css/all.css" rel="stylesheet">
+	<link href="font/fontawesome/css/all.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<!-- to nap and logo start here -->
+		<header>
+			<!-- top nav menu -->
+			<div class="top-nav">
+				<div class="social-media-icon">
+					<ul>
+						<li><a href="#" class="fab fa-facebook-f"></a></li>
+						<li><a href="#" class="fab fa-twitter"></a></li>
+						<li><a href="#" class="fab fa-instagram"></a></li>
+						<li><a href="#" class="fab fa-snapchat-ghost"></a></li>
+						<li><a href="#" class="fab fa-line"></a></li>
+						<li><a href="#" class="fas fa-user"></a></li>
+					</ul>
+				</div>
+			</div>
+			<!-- logo -->
+			<div class="logo">
+				<img src="">
+			</div>
+		</header>
+		<!-- to nap and logo end here -->
+
+		<!-- navbar -->
+		<nav class="navbar">
+			<ul>
+				<li><a href="#">HOME</a></li>
+				<li><a href="#">NEWS FEED</a></li>
+				<li><a href="#">CALCULATE</a></li>
+				<li><a href="#">ONLINE ZAKAT</a></li>
+				<li><a href="#">BLOG</a></li>
+				<li><a href="#" i class="fas fa-search"></a></li>				
+			    
+			</ul>
+		</nav>
+		<!-- navbar end -->
+
+		<!-- section one -->
+		<div class="section-one">
+			<div class="cal-zakat-bar column">
+				<div class="text-image-content">
+					<a href="#"><img src="img/test.jpg" width="300px">
+						<div class="image-text-block">
+							<h1>Nature</h1>
+    						<p>What a beautiful sunrise</p>
+						</div>
+					</a>
+			    </div>
+			</div>
+			<div class="pay-zakat-bar column">
+				<div class="text-image-content">
+					<a href="#"><img src="img/test.jpg" width="300px">
+						<div class="image-text-block">
+							<h1>Nature</h1>
+    						<p>What a beautiful sunrise</p>
+						</div>
+					</a>
+			    </div>
+			</div>
+			<div class="log-zakat-bar column">
+				<div class="text-image-content">
+					<a href="#"><img src="img/test.jpg" width="300px">
+						<div class="image-text-block">
+							<h1>Nature</h1>
+    						<p>What a beautiful sunrise</p>
+						</div>
+					</a>
+			    </div>
+			</div>
+		</div>
+		<!-- section one end-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</div>
+
+
+<script type="text/javascript" src="script/script.js"></script>
+</body>
+</html>
